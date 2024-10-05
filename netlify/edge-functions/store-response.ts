a4b9f3381eb8f53e58ba20f03ea84a579f28e17a
huuthienp <htp898@uowmail.edu.au>
2024-10-05 14:16:41 +1000

    feat: Handle pre-quiz and post-quiz responses
    - Store them separately
    - Checks quizType in JSON payload

--- netlify/edge-functions/store-response.ts
+++ netlify/edge-functions/store-response.ts
@@ -1,4 +1,4 @@
-import { getStore } from "@netlify/blobs";
+import { getStore, Store } from '@netlify/blobs'
 import type { Context } from "@netlify/edge-functions";
 
 export default async (req: Request, context: Context) => {
@@ -22,26 +22,32 @@ export default async (req: Request, context: Context) => {
     // Parse the JSON body from the request
     const data = await req.json();
 
-    // Get the store
-    const store = getStore(process.env.RESPONSE_STORE_NAME);
-
     // Check for missing keys
-    const EXPECTED_KEYS = ["recordedDate", "quizType", "responseId", "respondentEmail"];
+    const EXPECTED_KEYS = ["quizType"];
     const missingKeys = EXPECTED_KEYS.filter(key => !(key in data));
 
     if (missingKeys.length > 0) {
       return new Response(`Missing keys in JSON: ${missingKeys.join(', ')}`, { status: 400 });
     }
 
-    // Use response ID as key
-    const key = data.responseId;
+    // Extract quizType to get the store and the key
+    const { quizType } = data;
+    const { PRE_QUIZ_ID, POST_QUIZ_ID } = process.env;
+    let responses: Store, key: string;
+
+    if (quizType === "pre") {
+      responses = getStore(PRE_QUIZ_ID);
+      key = data.preResponseId;
+    } else if (quizType === "post") {
+      responses = getStore(POST_QUIZ_ID);
+      key = data.postResponseId;
+    } else {
+      const message = `Invalid quizType: ${quizType}`;
+      return new Response(message, { status: 400 });
+    }
 
     // Store the data in Netlify Blobs
-    await store.set(key, JSON.stringify({
-      recordedDate: data.recordedDate,
-      quizType: data.quizType,
-      respondentEmail: data.respondentEmail,
-    }));
+    await responses.set(key, JSON.stringify(data));
 
     return new Response(`Success: ${key}`, { status: 200 });
 
