8d526e71395182fdd1fa49b1132964229ce4ad54
huuthienp <htp898@uowmail.edu.au>
2024-10-07 06:53:14 +1100

    fix: Apply some web standards
    - accept GET instead of POST
    - auth using header instead of body
    - make error messages less verbose

--- netlify/functions/fetch-response.js
+++ netlify/functions/fetch-response.js
@@ -26,11 +26,11 @@ class CustomResponse extends Response {
 }
 
 
-export default async (req, context) => {
+export default async (req) => {
   try {
     // Ensure the request method is POST
     const methodUpper = req.method.toUpperCase();
-    if (methodUpper !== 'POST') {
+    if (methodUpper !== 'GET') {
       const message = `Method not allowed: ${methodUpper}.`;
       console.error(message);
       return new CustomResponse(message, 405);
@@ -42,8 +42,8 @@ export default async (req, context) => {
 
     // Validate quizType
     if (quizType === null || quizType === undefined) {
-      const message = `${url.href} is missing quiztype (case-insensitive, first occurence).`;
-      console.error(message);
+      const message = 'URL is missing quiztype (case-insensitive, first occurence).';
+      console.error(message, req.url);
       return new CustomResponse(message, 400);
 
     } else if (quizType !== 'pre' && quizType !== 'post') {
@@ -57,42 +57,42 @@ export default async (req, context) => {
     const quizTypeLower = quizType.toLowerCase();
 
     // Determine the survey ID based on quiz type
-    const { PRE_QUIZ_ID, POST_QUIZ_ID } = process.env;
+    const { ID_HEADER, PRE_QUIZ_ID, POST_QUIZ_ID } = process.env;
 
     const surveyId = (quizTypeLower === 'pre') ? PRE_QUIZ_ID : POST_QUIZ_ID;
 
-    const quizResponses = getStore(surveyId);
+    // Check if id exists in Netlify Blobs
+    const id = req.headers.get(ID_HEADER);
 
-    // Extract responseId from request body
-    const body = await req.json(); // error handled below
-
-    const { responseId } = body;
-
-    if (!responseId) {
-      const strBody = JSON.stringify(body);
-      const message = `${strBody} is missing a responseId (case-sensitive) or has an empty one.`;
-      console.error(message);
-      return new CustomResponse(message, 400);
+    if (!id) {
+      const message = 'Unauthorized.';
+      console.error(message, '\n', req.headers);
+      return new CustomResponse(message, 401);
     }
 
-    // Check if responseId exists in Netlify Blobs
-    const entry = await quizResponses.get(responseId, { consistency: 'strong' });
+    // Search Netlify Blobs for ID
+    const quizResponses = getStore(surveyId);
+
+    const entry = await quizResponses.get(id, { consistency: 'strong' });
 
     if (entry === null) {
-      const message = `${responseId} is not found in ${quizTypeLower}-quiz.`;
-      console.error(message);
-      return new CustomResponse(message, 404);
+      const message = `Invalid ID for ${quizTypeLower}-quiz.`;
+      console.error(message, id);
+      return new CustomResponse(message, 403);
     }
 
     // Prepare API request to Qualtrics
     const { QDC_ID, Q_API_TOKEN } = process.env;
 
-    const qUrl = `https://${QDC_ID}.qualtrics.com/API/v3/surveys/${surveyId}/responses/${responseId}`;
+    const qUrl = `https://${QDC_ID}.qualtrics.com/API/v3/surveys/${surveyId}/responses/${id}`;
 
     const options = {
-      method: 'GET',
+      method: methodUpper,
       url: qUrl,
-      headers: { Accept: 'application/json', 'X-API-TOKEN': Q_API_TOKEN },
+      headers: {
+        Accept: 'application/json',
+        'X-API-TOKEN': Q_API_TOKEN
+      },
     };
 
     // Make API request to Qualtrics
@@ -107,27 +107,21 @@ export default async (req, context) => {
     // Handle any errors that occur during the process
     if (error.response) {
       // The request was made and the server responded with a non-2xx status code
-      const message = error.response.data;
-      console.error(message);
+      const message = error.response.data.meta.error.errorMessage
+        || 'Server responded with error.';
+      console.error(message, '\n', error.response.data);
       return new CustomResponse(message, error.response.status);
 
     } else if (error.request) {
       // The request was made but no response was received
-      const message = 'No response from Qualtrics.';
-      console.error(message);
+      const message = 'No response, try again.';
+      console.error(message, '\n', error.request);
       return new CustomResponse(message, 503);
-
-    } else if (error instanceof SyntaxError &&
-      error.message.toLowerCase().includes('json')) {
-      // Handle error from req.json()
-      const body = await req.text();
-      const message = `${body} is not valid JSON.`;
-      return new CustomResponse(message, 400);
 
     } else {
       // Something happened in setting up the request that triggered an Error
-      const message = error.message || error;
-      console.error(message);
+      const message = 'Internal error.';
+      console.error(error);
       return new CustomResponse(message, 500);
     }
   }
