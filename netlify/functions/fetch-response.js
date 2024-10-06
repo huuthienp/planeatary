d15d62c56cca8afa5906c9e52018a3d84e7744be
huuthienp <htp898@uowmail.edu.au>
2024-10-07 06:58:21 +1100

    chore: Import CustomResponse

--- netlify/functions/fetch-response.js
+++ netlify/functions/fetch-response.js
@@ -1,29 +1,6 @@
 import axios from 'axios';
 import { getStore } from '@netlify/blobs';
-
-
-class CustomResponse extends Response {
-  constructor(body = 'Success', status = 200) {
-    const strBody = typeof body === 'string' ? body
-      : JSON.stringify(body);
-
-    let contentType;
-
-    try {
-      JSON.parse(strBody);
-      contentType = 'application/json';
-    } catch {
-      contentType = 'text/plain';
-    }
-
-    const options = {
-      status: status,
-      headers: { 'Content-Type': contentType }
-    };
-
-    super(strBody, options);
-  }
-}
+import { CustomResponse } from '../../classes/http.mjs';
 
 
 export default async (req) => {
