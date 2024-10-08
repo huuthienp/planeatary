f07e228104ca804d169b83d8c76e65d4ff6b4a83
huuthienp <htp898@uowmail.edu.au>
2024-10-08 17:16:42 +1100

    feat: Use dynamic parent and iframe origins

--- listen-to-qualtrics.js
+++ listen-to-qualtrics.js
@@ -1,8 +1,38 @@
+// /listen-to-qualtrics.js
+
+// Initialize a set to store allowed origins
+const allowedOrigins = new Set();
+
+
+// Function to add a new origin to the set
+function addOrigin(origin) {
+  allowedOrigins.add(origin);
+  console.log("Updated allowed origins:", Array.from(allowedOrigins));
+}
+
+
+// Listen for messages from the iframe
 window.addEventListener("message", (event) => {
-  if (event.origin == "https://uow.au1.qualtrics.com") {
+  // Check if the received message is requesting the origin
+  if (event.data === "getParentOrigin") {
+
+    // Add the origin of the message sender to the allowed list
+    addOrigin(event.origin);
+
+    // Prepare the response with the origin information
+    const response = {
+      type: "originResponse",
+      origin: window.location.origin
+    };
+
+    // Send the origin back to the iframe
+    // Note: event.origin specifies the origin of the iframe
+    event.source.postMessage(response, event.origin);
+
+  } else if (allowedOrigins.has(event.origin)) {
     try {
       // Parse the JSON data from the event
-      const data = JSON.parse(event.data);
+      const data = JSON.parse(event.data); // error handled below
 
       // Check if quizType exists in data
       if (!data.hasOwnProperty('quizType')) {
@@ -39,8 +69,13 @@ window.addEventListener("message", (event) => {
         }
       }
     } catch (error) {
-      console.error('\n', error.message || error);
-      // Optionally, handle the parsing error (e.g., log it, show user feedback)
+      if (error instanceof SyntaxError && error.message.toLowerCase().includes('json')) {
+        console.error('Cannot parse as JSON:', event.data);
+      } else {
+        console.error('\n', error.message || error);
+      }
     }
+  } else {
+    console.warn("Received message from unauthorized origin:", event.origin);
   }
 });
--- qualtrics-assets/send-data.js
+++ qualtrics-assets/send-data.js
@@ -1,35 +1,55 @@
 // /qualtrics-assets/send-data.js
 
+// Initialize
+let parentOrigin;
+
+const dataToSend = {
+  quizType: '${e://Field/quizType}',
+  responseId: '${e://Field/ResponseID}',
+  recordedDate: '${date://CurrentDate/c}',
+  totalScore: '${gr://SC_4OW9P7VDYuDLVbw/Score}',
+  pointArray: [
+    /* q01 */ '${q://QID3/SelectedChoicesRecode}',
+    /* q02 */ '${q://QID4/SelectedChoicesRecode}',
+    /* q03 */ '${q://QID5/SelectedChoicesRecode}',
+    /* q04 */ '${q://QID6/SelectedChoicesRecode}',
+    /* q05 */ '${q://QID7/SelectedChoicesRecode}',
+    /* q06 */ '${q://QID8/SelectedChoicesRecode}',
+    /* q07 */ '${q://QID10/SelectedChoicesRecode}', // Q7 was created 10th
+    /* q08 */ '${q://QID9/SelectedChoicesRecode}', // Q8 was created 9th
+    /* q09 */ '${q://QID11/SelectedChoicesRecode}',
+    /* q10 */ '${q://QID13/SelectedChoicesRecode}', // Q10 was created 13th
+    /* q11 */ '${q://QID12/SelectedChoicesRecode}', // Q11 was created 12th
+    /* q12 */ '${q://QID14/SelectedChoicesRecode}',
+  ]
+};
+
+
+Qualtrics.SurveyEngine.addOnReady(
+  function() {
+    /*Place your JavaScript here to run when the page is fully displayed*/
+    // Listen for the response from the parent window
+    window.addEventListener("message",
+      (event) => {
+        // Check if the received message is the origin response
+        if (event.data.type === "originResponse") {
+          // Log the received parent origin
+          console.log("Parent origin:", event.data.origin);
+
+          parentOrigin = event.data.origin;
+        }
+      }
+    );
+
+    // Send a message to the parent window requesting the origin
+    window.parent.postMessage("getParentOrigin", "*");
+  }
+);
+
 Qualtrics.SurveyEngine.addOnPageSubmit(
   function(type) {
     if (type == 'next') {
       /*Place your JavaScript here to run when the page is submitted*/
-
-      const parentOrigin = 'https://planeatary.netlify.app';
-
-      const dataToSend = {
-        quizType: '${e://Field/quizType}',
-        responseId: '${e://Field/ResponseID}',
-        recordedDate: '${date://CurrentDate/c}',
-        result: {
-          totalScore: '${gr://SC_4OW9P7VDYuDLVbw/Score}',
-          pointArray: [
-            /* q01 */ '${q://QID3/SelectedChoicesRecode}',
-            /* q02 */ '${q://QID4/SelectedChoicesRecode}',
-            /* q03 */ '${q://QID5/SelectedChoicesRecode}',
-            /* q04 */ '${q://QID6/SelectedChoicesRecode}',
-            /* q05 */ '${q://QID7/SelectedChoicesRecode}',
-            /* q06 */ '${q://QID8/SelectedChoicesRecode}',
-            /* q07 */ '${q://QID10/SelectedChoicesRecode}', // Q7 was created 10th
-            /* q08 */ '${q://QID9/SelectedChoicesRecode}', // Q8 was created 9th
-            /* q09 */ '${q://QID11/SelectedChoicesRecode}',
-            /* q10 */ '${q://QID13/SelectedChoicesRecode}', // Q10 was created 13th
-            /* q11 */ '${q://QID12/SelectedChoicesRecode}', // Q11 was created 12th
-            /* q12 */ '${q://QID14/SelectedChoicesRecode}',
-          ]
-        }
-      };
-
       window.parent.postMessage(JSON.stringify(dataToSend), parentOrigin);
     }
   }
