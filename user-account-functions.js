5186738769d8466181379a727377c4220b89c35f
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-01-07 09:12:27 +0700

    feat: confirm signup on hash change

--- user-account-functions.js
+++ user-account-functions.js
@@ -1,3 +1,21 @@
+async function confirmUser(identt, fragment) {  // not used
+    if (fragment === null) {
+        console.warn('Token is missing in attempt to confirm signup.');
+        return null;
+    }
+    const token = fragment.value;
+    try {
+        const response = await identt.confirm(token, true);
+        netlifyIdentity.open();
+        return response;
+    }
+    catch(e) {
+        console.error(e);
+        netlifyIdentity.open();
+        showFlashMessage('Email could not be confirmed.');
+        return null;
+    }
+}
 export function checkFragment(name) {
     let fragment = {};
     const _split = window.location.hash.substring(1).split('=');
@@ -9,4 +27,17 @@ export function checkFragment(name) {
     } else {
         return null;
     }
+}
+function showFlashMessage(message) {  // not used
+    const flashMessage = document.querySelector('div.modalContent div.flashMessage');
+    if (flashMessage === null) {
+        const container = document.createElement('div');
+        const header = document.querySelector('div.modalContent div.header') || document.querySelector('body');
+        container.classList.add('flashMessage', 'error');
+        container.textContent = message;
+        header.insertAdjacentHTML('afterEnd', container.outerHTML);
+    } else {
+        flashMessage.textContent = message;
+    }
+    return;
 }
