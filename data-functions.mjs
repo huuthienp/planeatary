59b709faafbc0128dd8f2cf1520405f7799f3b0c
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-01-21 14:33:06 +0700

    chore(mjs): clean and indent by 4

--- data-functions.mjs
+++ data-functions.mjs
@@ -1,67 +1,54 @@
 export function reformatQualtricsData(data, type) {
-  try {
-    let reformatted = {};
-
-    if (type === 'response') {
-      reformatted.quizType = data['quizType'];
-      reformatted.responseId = data['result']['responseId'];
-
-      const values = data['result']['values'];
-
-      reformatted.recordedDate = values['recordedDate'];
-
-      // Array of the desired keys
-      const orderedKeys = [
-        'QID3', 'QID4', 'QID5',
-        'QID6', 'QID7', 'QID8',
-        'QID10', 'QID9', 'QID11',
-        'QID13', 'QID12', 'QID14'];
-
-      const pointArray = orderedKeys.map(key => values[key] || 0);
-
-      reformatted.result = {
-        totalScore: values['SC_4OW9P7VDYuDLVbw'],
-        pointArray: pointArray
-      }
-
-    } else {
-      throw Error(`Empty or invalid data type: ${type}`);
+    try {
+        let reformatted = {};
+        if (type === 'response') {
+            reformatted.quizType = data['quizType'];
+            reformatted.responseId = data['result']['responseId'];
+            const values = data['result']['values'];
+            reformatted.recordedDate = values['recordedDate'];
+            // Array of the desired keys
+            const orderedKeys = [
+                'QID3', 'QID4', 'QID5',
+                'QID6', 'QID7', 'QID8',
+                'QID10', 'QID9', 'QID11',
+                'QID13', 'QID12', 'QID14'];
+            const pointArray = orderedKeys.map(key => values[key] || 0);
+            reformatted.result = {
+                totalScore: values['SC_4OW9P7VDYuDLVbw'],
+                pointArray: pointArray
+            }
+        } else {
+            throw Error(`Empty or invalid data type: ${type}`);
+        }
+        return reformatted;
+    } catch (error) {
+        if (error instanceof SyntaxError && error.message.toLowerCase().includes('json')) {
+            console.error('Cannot parse as JSON:', data);
+        } else {
+            console.error(error);
+        }
     }
-
-    return reformatted;
-
-  } catch (error) {
-    if (error instanceof SyntaxError && error.message.toLowerCase().includes('json')) {
-      console.error('Cannot parse as JSON:', data);
-    } else {
-      console.error(error);
-    }
-  }
 }
 
 
 export function saveLocalStorage(data, type) {
-
-  // Iterate over each key-value pair in the parsed data
-  for (const [key, value] of Object.entries(data)) {
-    if (type === 'response') {
-      const { quizType } = data;
-      try {
-        // Capitalize the first letter of the key
-        const titleCaseKey = key.charAt(0).toUpperCase() + key.slice(1);
-
-        // Process the value and store in localStorage
-        const valueAsStr = typeof value === 'string';
-        const strValue = valueAsStr ? value : JSON.stringify(value);
-
-        localStorage.setItem(`${quizType}${titleCaseKey}`, strValue);
-
-      } catch (storageError) {
-        console.error(`Cannot store ${key} as ${value}.`, '\n', storageError);
-        continue;
-      }
+    // Iterate over each key-value pair in the parsed data
+    for (const [key, value] of Object.entries(data)) {
+        if (type === 'response') {
+            const { quizType } = data;
+            try {
+                // Capitalize the first letter of the key
+                const titleCaseKey = key.charAt(0).toUpperCase() + key.slice(1);
+                // Process the value and store in localStorage
+                const valueAsStr = typeof value === 'string';
+                const strValue = valueAsStr ? value : JSON.stringify(value);
+                localStorage.setItem(`${quizType}${titleCaseKey}`, strValue);
+            } catch (storageError) {
+                console.error(`Cannot store ${key} as ${value}.`, '\n', storageError);
+                continue;
+            }
+        }
     }
-  }
 }
 
 
@@ -96,7 +83,7 @@ export async function updateTaskStatus(preResponseId, taskData) {
 
 
 export function mergeTaskArrays(data, separator = '@') {
-/**
+    /**
  * @param {Object} data - The input data object.
  * @returns {Object} An object containing an array named chosen.
  *
@@ -119,30 +106,28 @@ export function mergeTaskArrays(data, separator = '@') {
  *   // ... more numbered keys
  * }
  */
-  const chosen = [];
-  // const done = [];
-  // const time = [];
-
-  Object.values(data).forEach(question => {
-    question['tasks'].forEach(task => {
-      if (task.choice === 'chosen') {
-        const completed = task.status === 'Completed';
-        const finishTime = task.finishTime || ''; // in case undefined
-        const suffix = (completed || finishTime) ? separator+finishTime : '';
-        chosen.push(task.taskNumber + suffix);
-      }
-      // if (task.status === 'Completed') {
-        // done.push(task.taskNumber);
-      // }
-      // time.push(task.finish);
+    const chosen = [];
+    // const done = [];
+    // const time = [];
+    Object.values(data).forEach(question => {
+        question['tasks'].forEach(task => {
+            if (task.choice === 'chosen') {
+                const completed = task.status === 'Completed';
+                const finishTime = task.finishTime || ''; // in case undefined
+                const suffix = (completed || finishTime) ? separator+finishTime : '';
+                chosen.push(task.taskNumber + suffix);
+            }
+            // if (task.status === 'Completed') {
+            // done.push(task.taskNumber);
+            // }
+            // time.push(task.finish);
+        });
     });
-  });
-
-  return {
-    chosen: chosen,
-    // done: done,
-    // time: time
-  };
+    return {
+        chosen: chosen,
+        // done: done,
+        // time: time
+    };
 }
 
 
--- listen-to-qualtrics.mjs
+++ listen-to-qualtrics.mjs
@@ -1,55 +1,46 @@
 // /listen-to-qualtrics.mjs
-
 import { saveLocalStorage } from './data-functions.mjs';
-
 // Initialize a set to store allowed origins
 const allowedOrigins = new Set();
 
 
 // Function to add a new origin to the set
 function addOrigin(origin) {
-  allowedOrigins.add(origin);
-  console.log('Updated allowed origins:', Array.from(allowedOrigins));
+    allowedOrigins.add(origin);
+    console.log('Updated allowed origins:', Array.from(allowedOrigins));
 }
 
 
 // Listen for messages from the iframe
 window.addEventListener('message', (event) => {
-  // Check if the received message is requesting the origin
-  if (event.data === 'getParentOrigin') {
-
-    // Add the origin of the message sender to the allowed list
-    addOrigin(event.origin);
-
-    // Prepare the response with the origin information
-    const response = {
-      type: 'originResponse',
-      origin: window.location.origin
-    };
-
-    // Send the origin back to the iframe
-    // Note: event.origin specifies the origin of the iframe
-    event.source.postMessage(response, event.origin);
-
-  } else if (allowedOrigins.has(event.origin)) {
-    try {
-      // Parse the JSON data from the event
-      const data = JSON.parse(event.data); // error handled below
-
-      console.log('Parsed.', '\n', data);
-
-      saveLocalStorage(data, 'response');
-
-    } catch (error) {
-      if (error instanceof SyntaxError && error.message.toLowerCase().includes('json')) {
-        console.error('Cannot parse as JSON:', event.data);
-      } else {
-        console.error('\n', error.message || error);
-      }
+    // Check if the received message is requesting the origin
+    if (event.data === 'getParentOrigin') {
+        // Add the origin of the message sender to the allowed list
+        addOrigin(event.origin);
+        // Prepare the response with the origin information
+        const response = {
+            type: 'originResponse',
+            origin: window.location.origin
+        };
+        // Send the origin back to the iframe
+        // Note: event.origin specifies the origin of the iframe
+        event.source.postMessage(response, event.origin);
+    } else if (allowedOrigins.has(event.origin)) {
+        try {
+            // Parse the JSON data from the event
+            const data = JSON.parse(event.data); // error handled below
+            console.log('Parsed.', '\n', data);
+            saveLocalStorage(data, 'response');
+        } catch (error) {
+            if (error instanceof SyntaxError && error.message.toLowerCase().includes('json')) {
+                console.error('Cannot parse as JSON:', event.data);
+            } else {
+                console.error('\n', error.message || error);
+            }
+        }
+    } else {
+        console.warn('Received message without greeting.',
+            '\nFrom:', event.origin,
+            '\nMessage:', event.data);
     }
-  } else {
-    console.warn('Received message without greeting.',
-      '\nFrom:', event.origin,
-      '\nMessage:', event.data);
-  }
 });
