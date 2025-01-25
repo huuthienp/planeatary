6aa5c0800939f3214d4074a8cfe3eef5381eba06
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-01-25 21:05:18 +0700

    feat: fetch all past responses of user
    - return ordered array of cycles (pre-post pairs)
    - require token auth (logged in via GoTrue)
    - support `quiztype` param to fetch just either pre or post

--- netlify/functions/fetch-responses.mjs
+++ netlify/functions/fetch-responses.mjs
@@ -1,109 +1,59 @@
-import axios from 'axios';
-import { getStore } from '@netlify/blobs';
-import { CustomResponse } from '../../classes/http.mjs';
+import { CustomResponse } from '../../scripts/custom-classes.mjs';
+import { fetchData, fetchResponse, reformatResponseData } from '../../scripts/helpers.mjs';
 
 
-export default async (req) => {
-  try {
-    // Ensure the request method is POST
-    const methodUpper = req.method.toUpperCase();
-    if (methodUpper !== 'GET') {
-      const message = `Method not allowed: ${methodUpper}.`;
-      console.error(message);
-      return new CustomResponse(message, 405);
-    }
+export default async (request, context) => {
+    const query = {};
+    const { env } = process;
+    const data = [];  // an array of fetched responses grouped by cycle and quiz type
+    query.length = 0;
+    query.lengthOk = 0;
+    try {  // authenticate request by token and fetch all responses
+        const requestUrl = new URL(request.url.toLowerCase());
+        query.requestedTypes = requestUrl.searchParams.get('quiztype') || ['pre', 'post'];
+        const targetUrl = context.site.url + '/.netlify/identity/user';
+        const nf_jwt = context.cookies.get('nf_jwt');
+        const authHeader = nf_jwt ? `Bearer ${nf_jwt}` : request.headers.get('Authorization');
+        if (!authHeader) {  // when neither nf_jwt cookie nor auth header can be found
+            return new CustomResponse('Unauthorised!', 401);
+        }  // else, send token for verification
+        const { id, user_metadata: { responseHistory } } = await fetchData(targetUrl, {
+            method: 'GET',
+            headers: { Authorization: authHeader },
+        });  // get id of user and their response history
+        console.log(`${id} authorised for fetching responses!`);
+        for (let cycle of responseHistory) {
+            const { preId, postId } = cycle;
+            const preResponse = await tryDecideFetchResponse(query, 'pre', preId, env);
+            const postResponse = await tryDecideFetchResponse(query, 'post', postId, env);
+            data.push(({ preResponse, postResponse }));
+        }  // end of fetching all cycles
+        console.log('Fetching is done!', query.lengthOk, '/', query.length);
+        return new CustomResponse(data, query.lengthOk === query.length ? 200 : query.lengthOk === 0 ? 404 : 207);
+    } catch(breakError) {  // e.g. invalid token
+        console.warn('Caught', breakError);
+        const { message, name, stack, status } = breakError;
+        return new CustomResponse(stack || name + ': ' + message, status || 500);
+    }  // end of catching breaking error, which interrupts fetching
+};
 
-    // Extract quizType from query parameters
-    const url = new URL(req.url.toLowerCase());
-    const quizType = url.searchParams.get('quiztype');
 
-    // Validate quizType
-    if (quizType === null || quizType === undefined) {
-      const message = 'URL is missing quiztype (case-insensitive, first occurence).';
-      console.error(message, req.url);
-      return new CustomResponse(message, 400);
+export const config = { method: 'GET', path: '/api/fetch-responses' };
 
-    } else if (quizType !== 'pre' && quizType !== 'post') {
-      const emptyQuizType = quizType === '';
-      const message = emptyQuizType ? `Empty quiz type.` : `${quizType} is an invalid quiz type.`;
-      console.error(message);
-      return new CustomResponse(message, 400);
-    }
 
-    // Convert quizType to lowercase
-    const quizTypeLower = quizType.toLowerCase();
-
-    // Determine the survey ID based on quiz type
-    const { ID_HEADER, PRE_QUIZ_ID, POST_QUIZ_ID } = process.env;
-
-    const surveyId = (quizTypeLower === 'pre') ? PRE_QUIZ_ID : POST_QUIZ_ID;
-
-    // Check if id exists in Netlify Blobs
-    const id = req.headers.get(ID_HEADER);
-
-    if (!id) {
-      const message = 'Unauthorized.';
-      console.error(message, '\n', req.headers);
-      return new CustomResponse(message, 401);
-    }
-
-    // Search Netlify Blobs for ID
-    const quizResponses = getStore(surveyId);
-
-    const entry = await quizResponses.get(id, { consistency: 'strong' });
-
-    if (entry === null) {
-      const message = `Invalid ID for ${quizTypeLower}-quiz.`;
-      console.error(message);
-      return new CustomResponse(message, 403);
-    }
-
-    // Prepare API request to Qualtrics
-    const { QDC_ID, Q_API_TOKEN } = process.env;
-
-    const qUrl = `https://${QDC_ID}.qualtrics.com/API/v3/surveys/${surveyId}/responses/${id}`;
-
-    const options = {
-      method: methodUpper,
-      url: qUrl,
-      headers: {
-        Accept: 'application/json',
-        'X-API-TOKEN': Q_API_TOKEN
-      },
-    };
-
-    // Make API request to Qualtrics
-    const { data } = await axios.request(options);
-    // axios throws error when status code is not 2xx
-
-    // Return successful response
-    console.log(`A response is fetched.`);
-    return new CustomResponse(data); // default status is 200
-
-  } catch (error) {
-    // Handle any errors that occur during the process
-    if (error.response) {
-      // The request was made and the server responded with a non-2xx status code
-      const message = error.response.data.meta.error.errorMessage
-        || 'Server responded with error.';
-      console.error(message, '\n', error.response.data);
-      return new CustomResponse(message, error.response.status);
-
-    } else if (error.request) {
-      // The request was made but no response was received
-      const message = 'No response, try again.';
-      console.error(message, '\n', error.request);
-      return new CustomResponse(message, 503);
-
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      const message = 'Internal error.';
-      console.error(error);
-      return new CustomResponse(message, 500);
-    }
-  }
-  // end of try-catch
+async function tryDecideFetchResponse(query, quizType, responseId, env) {
+    try {  // decide whether to fetch a quiz response
+        if (query.requestedTypes.includes(quizType) && responseId) {
+            query.length += 1;
+            const fetchedResponse = reformatResponseData(await fetchResponse(responseId, quizType, env));
+            query.lengthOk +=1;
+            console.log(responseId, 'is fetched!');
+            return fetchedResponse;
+        } else {  // when the request specifies just either 'pre' or 'post'
+            return null;  // skip fetching
+        }  //  or when response ID is not found in history (user has not done post-quiz)
+    } catch (fetchError) {  // not breaking, moving on to next quiz type or cycle
+        console.warn('Caught', fetchError);
+        return ({ fetchError });
+    }  // end of try deciding to fetch a response
 }
-
-
-export const config = { path: '/api/fetch-response-aws' }
