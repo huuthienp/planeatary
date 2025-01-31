3122282f1082de633c1562d117789c864009dc41
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-02-01 01:15:29 +0700

    chore(admin-gotrue): add content type to response

--- netlify/functions/admin-gotrue.cjs
+++ netlify/functions/admin-gotrue.cjs
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     try {  // try verifying token and processing request to admin
-        const { body, headers: { authorization, user_id }, httpMethod } = event;
+        const { body: eventBody, headers: { authorization, user_id }, httpMethod } = event;
         const { identity } = context.clientContext;
         const { SECRET_AUTH_HEADER } = process.env;
         if (authorization !== SECRET_AUTH_HEADER) {
@@ -10,15 +10,20 @@ exports.handler = async function (event, context) {
                 statusCode: 401,
             };  // skip request to admin
         }  // end of function if unauthorized
-        const response = await fetch(`${identity.url}/admin/users/${user_id || undefined}`, {  // prevent getting all users
+        const response = await fetch(`${identity.url}/admin/users/${user_id || undefined}`/* prevent getting all users */, {
             method: httpMethod,
             headers: { Authorization: `Bearer ${identity.token}` },
-            body: JSON.stringify(body ? JSON.parse(body) : undefined),
-        });  // support GET, POST, PUT, DELETE
-        const { ok, status } = response;
+            body: JSON.stringify(eventBody ? JSON.parse(eventBody) : undefined),
+        });  // support GET, POST, PUT, DELETE a user
+        const { ok, status: statusCode } = response;
         const __log = ok ? console.log : console.error;
-        __log('Response:', status);
-        return { body: await response.text(), statusCode: status };
+        __log('Response:', statusCode);
+        let body = await response.text(), headers;
+        try {
+            JSON.parse(body);
+            headers = { 'Content-Type': 'application/json' };
+        } catch(parseError) { headers = { 'Content-Type': 'text/plain' }; }
+        return ({ body, headers, statusCode });
     } catch(internalError) {
         console.error(internalError);
         internalError.statusCode ??= 500;  // nullish coalescing operator (??) since ES11 (2020)
