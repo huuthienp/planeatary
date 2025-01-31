e73c9affdd86fa33658ac947b4bfc48416bac1ef
huuthienp <>
2025-01-31 18:54:35 +0700

    fix(admin-gotrue): rewrite
    - change auth
    - catch internal error

--- netlify/functions/admin-gotrue.cjs
+++ netlify/functions/admin-gotrue.cjs
@@ -1,27 +1,27 @@
 exports.handler = async function (event, context) {
-    const { body, headers, httpMethod } = event;
-    const { identity, user } = context.clientContext;
-    const { ADMIN_UUID } = process.env;
-    if (!user || user.sub != ADMIN_UUID) {
-        console.error('Unauthorized');
-        return {
-            body: JSON.stringify({ error: 'Unauthorized' }),
-            statusCode: 401,
-        };
-    }
-    const targetUrl = `${identity.url}/admin/users/${headers.user_id}`;
-    const authHeader = `Bearer ${identity.token}`;
-    const data = body ? JSON.parse(body) : undefined;
-    const response = await fetch(targetUrl, {
-        method: httpMethod,
-        headers: { Authorization: authHeader },
-        body: JSON.stringify(data),
-    });
-    const { status } = response;
-    const __log = response.ok ? console.log : console.error;
-    __log(status);
-    return {
-        body: await response.text(),
-        statusCode: status,
-    };
+    try {  // try verifying token and processing request to admin
+        const { body, headers: { authorization, user_id }, httpMethod } = event;
+        const { identity } = context.clientContext;
+        const { SECRET_AUTH_HEADER } = process.env;
+        if (authorization !== SECRET_AUTH_HEADER) {
+            console.error('Unauthorized!');
+            return {  // imitate response from identity endpoints
+                body: JSON.stringify({ code: 401, msg: 'Unauthorized' }),
+                statusCode: 401,
+            };  // skip request to admin
+        }  // end of function if unauthorized
+        const response = await fetch(`${identity.url}/admin/users/${user_id}`, {
+            method: httpMethod,
+            headers: { Authorization: `Bearer ${identity.token}` },
+            body: JSON.stringify(body ? JSON.parse(body) : undefined),
+        });  // support GET, POST, PUT, DELETE
+        const { ok, status } = response;
+        const __log = ok ? console.log : console.error;
+        __log('Response:', status);
+        return { body: await response.text(), statusCode: status };
+    } catch(internalError) {
+        console.error(internalError);
+        internalError.statusCode ??= 500;  // nullish coalescing operator (??) since ES11 (2020)
+        return internalError;
+    }  // end of function when internal error occurs
 };
