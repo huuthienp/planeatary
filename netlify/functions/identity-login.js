4c4b504755c98409ec3cd6d63b00f384885f6ccd
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-02-01 14:04:08 +0700

    feat(identity-login): add last login time

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -1,6 +1,8 @@
 // https://docs.netlify.com/functions/functions-and-identity/#trigger-functions-on-identity-events
 export async function handler(event) {
-    const body = event.body.user;  // can modify user data
+    const { user } = JSON.parse(event.body);  // can modify user data
+    user.app_metadata.last_login_at = (new Date()).toISOString();
+    const body = JSON.stringify(user);
     let statusCode = 200;  // can block event with non-2xx status
     return ({ body, statusCode });
 }  // end of login-event function
