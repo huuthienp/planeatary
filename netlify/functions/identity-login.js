c773f6353a6918f7d48264120065af527b5ebf59
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-02-12 20:43:05 +0700

    fix(identity): set response history as empty array on login event
    - complement b9bee8c0e1b46e6c56c391d0c90d59574c4a1fb9

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -2,6 +2,7 @@
 export async function handler(event) {
     const { user } = JSON.parse(event.body);  // can modify user data
     user.app_metadata.last_login_at = (new Date()).toISOString();
+    user.user_metadata.responseHistory ??= [];  // set to empty array if undefined or null
     const body = JSON.stringify(user);
     let statusCode = 200;  // can block event with non-2xx status
     return ({ body, statusCode });
