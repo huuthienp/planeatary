e440bef8f126fee71b6674c3508ab78676a805d7
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-02-13 19:15:32 +0700

    feat(identity): reject missing user metadata fields
    - no validation

--- netlify/functions/identity-validate.js
+++ netlify/functions/identity-validate.js
@@ -1,18 +1,25 @@
 import { isDisposableEmail } from 'disposable-email-domains-js';
 
-export async function handler(event) {
-    const user = JSON.parse(event.body).user;
-    const { email } = user;
-    const domain = email.substring(email.indexOf('@') + 1)
-    const spam = isDisposableEmail(email);
-    if (spam) {
-        console.error(domain, 'can be disposable:', spam.toString().toUpperCase());
-        return {
-            statusCode: 400,
-        }
-    };
-    return {
-        body: JSON.stringify(user),
-        statusCode: 200,
-    };
-}
+
+export async function handler(event) {  // check for spam and missing metadata
+    const { user } = JSON.parse(event.body);
+    const { email, user_metadata: userMetaData } = user;
+    const missingMetaData = validateMetaData(userMetaData ?? {});
+    let body, error;
+    let statusCode = 400;  // reject signup by default
+    if (isDisposableEmail(email)) {
+        body = error = 'Email may be disposable!';
+    } else if (missingMetaData.length > 0) {
+        body = error = `Metadata missing: ${missingMetaData.join(', ')}!`;
+    } else {  // approve signup
+        body = JSON.stringify(user);
+        statusCode = 200;
+    }  // end of checking
+    console.log(email, error ?? 'Approved!');
+    return ({ body, statusCode });
+}  // end of handler v2
+
+
+function validateMetaData(data, requiredKeys = ['age', 'gender', 'status']) {
+    return missingKeys = requiredKeys.filter(key => !Object.hasOwn(data, key));
+}  // end of validateMetaData v1
