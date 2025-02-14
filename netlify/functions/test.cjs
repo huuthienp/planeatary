1541342398f976f1e66fe475bb15c681a790ed1f
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-02-14 21:23:14 +0700

    chore(build): prepare for resetting password
    - use bun.lock (1.2.2)
    - add bcryptjs

--- netlify/functions/test.cjs
+++
@@ -1,6 +0,0 @@
-// exports.handler = async function (event, context) {
-//     const body = 'hi';
-//     const headers = {};
-//     const statusCode = 200;
-//     return ({body, headers, statusCode});
-// };
