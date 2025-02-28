5baee91bc9f6b214641e589c79fef719f5a3bb59
DreamThamonwan <121571272+DreamThamonwan@users.noreply.github.com>
2025-03-01 02:53:15 +1100

    Fix ui components
    -change styling in disabled buttons
    -change font-size in task cards
    -change styling in empty state

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -57,6 +57,19 @@ export function updateButton(button, color='buttonface', cursor='default') {
     button.style.backgroundColor = color;
     button.style.borderColor = color;
     button.style.cursor = cursor;
+    button.style.borderRadius = '0.5em';
+    if (cursor == "not-allowed") {
+        button.style.color = 'rgb(9, 59, 48)';
+        button.style.boxShadow = 'none';
+        button.style.opacity = 0.4;
+        button.style.pointerEvents = "none";
+    } else {
+        button.style.color = 'white';
+        button.style.boxShadow = '0 .125rem .25rem rgba(0, 0, 0, .075)';
+        button.style.opacity = 1;
+        button.style.pointerEvents = "auto";
+    }
+    
 }
 
 
