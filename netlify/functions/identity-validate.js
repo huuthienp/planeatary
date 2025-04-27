36834dcbf1bcf963333b024f93f33b8096120e73
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-04-27 17:11:04 +1000

    fix(identity-validate): v3
    - move required keys to env
    - rewrite handler
    - rename validateMetaData as checkKeys

--- netlify/functions/identity-validate.js
+++ netlify/functions/identity-validate.js
@@ -1,25 +1,34 @@
 import { isDisposableEmail } from 'disposable-email-domains-js';
 
+const requiredMtdtKeys = JSON.parse(process.env.REQUIRED_USER_METADATA_KEYS);
 
-export async function handler(event) {  // check for spam and missing metadata
-    const { user } = JSON.parse(event.body);
-    const { email, user_metadata: userMetaData } = user;
-    const missingMetaData = validateMetaData(userMetaData ?? {});
-    let body, error;
-    let statusCode = 400;  // reject signup by default
-    if (isDisposableEmail(email)) {
-        body = error = 'Email may be disposable!';
-    } else if (missingMetaData.length > 0) {
-        body = error = `Metadata missing: ${missingMetaData.join(', ')}!`;
-    } else {  // approve signup
-        body = JSON.stringify(user);
-        statusCode = 200;
-    }  // end of checking
-    console.log(email, error ?? 'Approved!');
-    return ({ body, statusCode });
-}  // end of handler v2
+// function handler (must be lambda-compatible)
+export async function handler(event) {  // v3
+    const response = {};
+    const payload = JSON.parse(event.body);
+    payload.errors = [];
+    // https://docs.netlify.com/functions/functions-and-identity/#trigger-functions-on-identity-events
+    // check for disposable domain and required user metadata
+    const { email, user_metadata } = payload.user;
+    if (isDisposableEmail(email)) payload.errors.push('\'' + email + '\' may be disposable');
+    const missingMtdt = checkKeys(user_metadata ?? {});
+    if (0 < missingMtdt.length) payload.errors.push('missing: ' + missingMtdt.join(','));
+    const approved = 0 === payload.errors.length;
+    if (approved) {  // approve sign-up
+        response.body = JSON.stringify(payload.user);
+        response.statusCode = 200;
+        console.log(payload.event, 'Approved!');
+    } else {  // reject sign-up
+        const code = 400;  // non-2xx rejects sign-up
+        const msg = payload.errors;
+        response.body = JSON.stringify({ code, msg });
+        response.statusCode = code;
+        console.error(payload.event, 'Rejected:', payload.errors.join('; '));
+    }  // payload is checked and response is created
+    return response;
+}  // function handler v3
 
 
-function validateMetaData(data, requiredKeys = ['age', 'gender', 'status']) {
-    return missingKeys = requiredKeys.filter(key => !Object.hasOwn(data, key));
-}  // end of validateMetaData v1
+function checkKeys(dataObject, keys = requiredMtdtKeys) {
+    return keys.filter(key => !Object.hasOwn(dataObject, key));
+}  // checkKeys v1.1
