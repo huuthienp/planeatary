5f0dacddae8babf62b2dd52e5f730850302482f4
huuthienp <168893925+huuthienp@users.noreply.github.com>
2025-09-18 18:01:45 +1000

    fix: "invalid date" in post-quiz reminder
    - `Math.max("Skipped", ...)` returns NaN

--- archive/qualtrics/prepare-post-quiz-reminder.js
+++ archive/qualtrics/prepare-post-quiz-reminder.js
@@ -1,7 +1,8 @@
 function codeTask() {
     /* Process data for post-quiz reminder */
     /* Beware: Piped text of empty array is empty string */
-    const unixTimes = JSON.parse(`~{aed://time}` || '[]');
+    let unixTimes = JSON.parse(`~{aed://time}` || '[]');
+    unixTimes = unixTimes.map(item => Number(item)).filter(num => !isNaN(num)); // remove "Skipped"
     const largest = Math.max(...unixTimes);
     const latestDate = new Date(largest);
     const latestDateString = latestDate.toLocaleString('en-AU', {
