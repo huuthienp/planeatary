42a4d924c46ddb9fd5f833dd64dc554ea8178915
huuthienp <168893925+huuthienp@users.noreply.github.com>
2026-05-03 14:46:41 +0700

    remove archive/ see f22bd6f5f39d0ec5d4097c4b59b71d136066e9da

--- archive/qualtrics/update-pre-id-for-user.js
+++
@@ -1,7 +0,0 @@
-function codeTask() {
-    // const fetchedResponseHistory = ~{ch://XXXX_xxxx/$.user_metadata.responseHistory};  // qualtrics piped text (censored)
-    fetchedResponseHistory.push({
-        preId: '${rm://Field/ResponseID}'
-    });
-    return { newData: { responseHistory: fetchedResponseHistory } };
-}
