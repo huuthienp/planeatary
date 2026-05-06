bf4c447e56af04a952a528c5e558766a5cef5308
huuthienp <168893925+huuthienp@users.noreply.github.com>
2026-05-06 22:39:42 +0700

    Add main container/tag i.e. where to mount main Vue app

---
+++ assets/main.mjs
@@ -0,0 +1,32 @@
+import { createApp } from 'vue';
+import { insertStyleLink } from 'vuethienp';
+
+const { url } = import.meta;
+const names = [
+]; // prettier-ignore
+
+const app = createApp({
+  mounted() {
+    //
+  },
+  setup() {
+    //
+  },
+  template: names.map(n_ => `<${n_}></${n_}>`).join(''),
+});
+
+for (const name of names) {
+  app.component(name, {
+    mounted() {
+      const cssUrl = new URL(`${name}.css`, url);
+      fetch(cssUrl) // cache to be used by link below
+        .then(res => res.ok && insertStyleLink(cssUrl))
+        .catch(err => console.warn(err));
+    },
+    props: [],
+    setup() {},
+    template: await (await fetch(new URL(`${name}.html`, url))).text(),
+  });
+}
+
+app.mount('main');
