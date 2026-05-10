b398ee9e950dca392d6ba5069ecab6354d0c1f97
huuthienp <168893925+huuthienp@users.noreply.github.com>
2026-05-10 14:52:43 +0700

    Add createAppFromElements

--- assets/app.mjs
+++ assets/app.mjs
@@ -1,7 +1,5 @@
-import { createApp } from 'vue';
-import { insertStyleLink } from 'vuethienp';
-import { insertStyleBlock } from 'vuethienp';
 import INDEX_UI from './index-ui.mjs';
+import { createAppFromElements } from 'vuethienp';
 
 const { url } = import.meta;
 
@@ -23,50 +21,11 @@ const names = [
   'development',
 ]; // prettier-ignore
 
-const app = createApp({
-  mounted() {
-    //
-  },
-  setup() {
-    //
-  },
-  template: names.map(n_ => `<${n_}></${n_}>`).join(''),
-});
-
-for (const name of names) {
-  const templateFetch = await fetch(new URL(`${name}.html`, url));
-  const fallbackElement = ELEMENTS[name];
-
-  if (!templateFetch.ok) {
-    if (fallbackElement) {
-      console.log(`OK '${name}' is defined`);
-    } else {
-      console.warn(`SKIPPED '${name}' is ${templateFetch.status} and undefined`);
-      continue;
-    }
-  }
-
-  app.component(name, {
-    mounted() {
-      /** INSERT STYLE
-       * {name}.html found -> insert link to {name}.css
-       * else -> {name} defined -> insert attached style as block
-       */
-
-      if (!templateFetch.ok) {
-        fallbackElement?.style && insertStyleBlock(fallbackElement.style.trim());
-        return;
-      }
-
-      const cssUrl = new URL(`${name}.css`, url);
-      fetch(cssUrl) // cache to be used by link below
-        .then(res => res.ok && insertStyleLink(cssUrl))
-        .catch(err => console.warn(err));
-    },
-    props: [],
-    setup() {},
-    template: (templateFetch.ok ? await templateFetch.text() : fallbackElement.template).trim(),
-  });
-}
-
-app.mount('main');
+( await createAppFromElements(
+  {
+    elements: ELEMENTS,
+    names,
+    url,
+  } /* better-diff */,
+  {},
+) ).mount('main');
--- assets/vuethienp.mjs
+++ assets/vuethienp.mjs
@@ -1,5 +1,61 @@
 import { createApp } from 'vue';
 
+export async function createAppFromElements(
+  /* prettier-ignore */ {
+    elements=Object.create(null),
+    names=['names-is-empty'],
+    url,
+  },
+  /* prettier-ignore */ {
+    mounted = function(){},
+    setup = function(){},
+  } = {},
+) {
+  const app = createApp({
+    mounted,
+    setup,
+    template: names.map(n_ => `<${n_}></${n_}>`).join(''),
+  });
+
+  for (const name of names) {
+    const templateFetch = await fetch(new URL(`${name}.html`, url));
+    const fallbackElement = elements[name];
+
+    if (!templateFetch.ok) {
+      if (fallbackElement) {
+        console.log(`OK '${name}' is defined`);
+      } else {
+        console.warn(`SKIPPED '${name}' is ${templateFetch.status} and undefined`);
+        continue;
+      }
+    }
+
+    app.component(name, {
+      mounted() {
+        /** INSERT STYLE
+         * {name}.html found -> insert link to {name}.css
+         * else -> {name} defined -> insert attached style as block
+         */
+
+        if (!templateFetch.ok) {
+          fallbackElement?.style && insertStyleBlock(fallbackElement.style.trim());
+          return;
+        }
+
+        const cssUrl = new URL(`${name}.css`, url);
+        fetch(cssUrl) // cache to be used by link below
+          .then(res => res.ok && insertStyleLink(cssUrl))
+          .catch(err => console.warn(err));
+      },
+      props: [],
+      setup() {},
+      template: (templateFetch.ok ? await templateFetch.text() : fallbackElement.template).trim(),
+    });
+  }
+
+  return app;
+} /* function createAppFromElements */
+
 export function insertStyleBlock(textContent) {
   document.head.appendChild(Object.assign(document.createElement('style'), { textContent }));
 } /* function insertStyleBlock */
